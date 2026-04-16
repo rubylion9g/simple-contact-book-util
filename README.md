50305f20a1226d8595cc28e44c7bd22e11cb0cb4
Emery Turner <marylouisebalcitis9517210@gmail.com>
2026-04-16 19:03:02 -0400

    Initial commit: contact book

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# simple-contact-book-util
-A simple contact book project
+# Contact Book
+
+A simple contact book project.
+\n
+## Getting Started
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Contact Book - Main module.
+"""
+
+def main():
+    print("Contact Book is running...")
+    print("Version 0.1.0")
+
+if __name__ == "__main__":
+    main()
